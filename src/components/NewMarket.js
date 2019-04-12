db5970e730203c466a8cd127a1c51ea618e615ae
suren <savetis.dev@gmail.com>
2019-04-12 14:13:32 +0300

    finish shop

---
+++ src/components/NewMarket.js
@@ -0,0 +1,130 @@
+import React from 'react';
+import { API, graphqlOperation } from 'aws-amplify';
+import { createMarket } from '../graphql/mutations';
+import { Form, Button, Dialog, Input, Select, Notification } from 'element-react';
+import { UserContext } from '../App';
+
+class NewMarket extends React.Component {
+  state = {
+    name: '',
+    tags: ['Arts', 'Web Dev', 'Technology', 'Crafts', 'Entertainment'],
+    selectedTags: [],
+    options: [],
+    addMarketDialog: false
+  };
+
+  handleAddMarket = async user => {
+    try {
+      this.setState({ addMarketDialog: false });
+      const input = {
+        name: this.state.name,
+        tags: this.state.selectedTags,
+        owner: user.username
+      };
+
+      const result = await API.graphql(graphqlOperation(createMarket, { input }));
+
+      console.log({ result });
+      console.info(`Created market: id ${result.data.createMarket.id}`);
+
+      this.setState({ name: '', selectedTags: [] });
+    } catch (err) {
+      console.error('Error adding new market', err);
+      Notification.error({
+        title: 'Error',
+        message: `${err.message || 'Error adding market'}`
+      });
+    }
+  };
+
+  handleFilterTags = query => {
+    const options = this.state.tags
+      .map(tag => ({ value: tag, label: tag }))
+      .filter(tag => tag.label.toLowerCase().includes(query.toLowerCase()));
+
+    this.setState({ options });
+  };
+
+  render() {
+    const { addMarketDialog, name } = this.state;
+    return (
+      <UserContext.Consumer>
+        {({ user }) => (
+          <>
+            <div className="market-header">
+              <h1 className="market-title">
+                Create Your MarketPlace
+                <Button
+                  type="text"
+                  icon="edit"
+                  className="market-title-button"
+                  onClick={() => this.setState({ addMarketDialog: true })}
+                />
+              </h1>
+
+              <Form inline={true} onSubmit={this.props.handleSearch}>
+                <Form.Item>
+                  <Input
+                    placeholder="Search Markets..."
+                    icon="circle-cross"
+                    onIconClick={this.props.handleClearSearch}
+                    value={this.props.searchTerm}
+                    onChange={this.props.handleSearchChange}
+                  />
+                </Form.Item>
+                <Form.Item>
+                  <Button type="info" icon="search" loading={this.props.isSearching} onClick={this.props.handleSearch}>
+                    Search
+                  </Button>
+                </Form.Item>
+              </Form>
+            </div>
+
+            <Dialog
+              title="Create New Market"
+              visible={addMarketDialog}
+              onCancel={() => this.setState({ addMarketDialog: false })}
+              size="large"
+              customClass="dialog"
+            >
+              <Dialog.Body>
+                <Form labelPosition="top">
+                  <Form.Item label="Add Market Name">
+                    <Input
+                      placeholder="Market Name"
+                      trim={true}
+                      onChange={name => this.setState({ name })}
+                      value={name}
+                    />
+                  </Form.Item>
+                  <Form.Item label="Add Tags">
+                    <Select
+                      placeholder="Market Tags"
+                      filterable
+                      multiple
+                      remote
+                      remoteMethod={this.handleFilterTags}
+                      onChange={selectedTags => this.setState({ selectedTags })}
+                    >
+                      {this.state.options.map(option => (
+                        <Select.Option key={option.value} label={option.label} value={option.value} />
+                      ))}
+                    </Select>
+                  </Form.Item>
+                </Form>
+              </Dialog.Body>
+              <Dialog.Footer>
+                <Button onClick={() => this.setState({ addMarketDialog: false })}>Cancel</Button>
+                <Button disabled={!name} onClick={() => this.handleAddMarket(user)}>
+                  Add
+                </Button>
+              </Dialog.Footer>
+            </Dialog>
+          </>
+        )}
+      </UserContext.Consumer>
+    );
+  }
+}
+
+export default NewMarket;
---
+++ src/components/PayButton.js
@@ -0,0 +1,113 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+import { API, graphqlOperation } from 'aws-amplify';
+import { getUser } from '../graphql/queries';
+import { createOrder } from '../graphql/mutations';
+import { Notification, Message } from 'element-react';
+import { history } from '../App';
+
+const stripeConfig = {
+  currency: 'USD',
+  publishableAPIKey: 'pk_test_3Tzrwffx7hzqgOWDu7XO4qkQ'
+};
+
+const PayButton = ({ product, userAttributes }) => {
+  const getOwnerEmail = async ownerId => {
+    try {
+      const input = { id: ownerId };
+
+      const result = await API.graphql(graphqlOperation(getUser, input));
+
+      return result.data.getUser.email;
+    } catch (err) {
+      console.log(`Error fetching product owner's email`, err);
+    }
+  };
+
+  const createShippingAddress = source => ({
+    city: source.address_city,
+    country: source.address_country,
+    address_line1: source.address_line1,
+    address_state: source.address_state,
+    address_zip: source.address_zip
+  });
+
+  const handleCharge = async token => {
+    try {
+      const ownerEmail = await getOwnerEmail(product.owner);
+
+      console.log({ ownerEmail });
+
+      const result = await API.post('orderlambda', '/charge', {
+        body: {
+          token,
+          charge: {
+            currency: stripeConfig.currency,
+            amount: product.price,
+            description: product.description
+          },
+          email: {
+            customerEmail: userAttributes.email,
+            ownerEmail,
+            shipped: product.shipped
+          }
+        }
+      });
+
+      if (result.charge.status === 'succeeded') {
+        let shippingAddress = null;
+        if (product.shipped) {
+          shippingAddress = createShippingAddress(result.charge.source);
+        }
+
+        const input = {
+          orderUserId: userAttributes.sub,
+          orderProductId: product.id,
+          shippingAddress
+        };
+
+        console.log('input', input);
+        const order = await API.graphql(graphqlOperation(createOrder, { input }));
+        console.log({ order });
+        Notification({
+          title: 'Success',
+          message: `${result.message}`,
+          type: 'success',
+          duration: 3000
+        });
+        setTimeout(() => {
+          history.push('/');
+          Message({
+            type: 'info',
+            message: 'Check your verified email for order details',
+            duration: 5000,
+            showClose: true
+          });
+        }, 3000);
+      }
+    } catch (err) {
+      console.error(err);
+      Notification.error({
+        title: 'Error',
+        message: `${err.message || 'Error processing order'}`
+      });
+    }
+  };
+
+  return (
+    <StripeCheckout
+      token={handleCharge}
+      email={userAttributes.email}
+      name={product.description}
+      amount={product.price}
+      currency={stripeConfig.currency}
+      stripeKey={stripeConfig.publishableAPIKey}
+      shippingAddress={product.shipped}
+      billingAddress={product.shipped}
+      locale="auto"
+      allowRememberMe={false}
+    />
+  );
+};
+
+export default PayButton;
